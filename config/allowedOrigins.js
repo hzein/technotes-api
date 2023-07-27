0d5fcc9766973aa982ce371ed506a1a7c0347ab9
hzein <hzein286@gmail.com>
2023-07-26 23:42:05 -0700

    Updated cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ["https://technotes-2q3j.onrender.com"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://technotes-2q3j.onrender.com",
+];
 
 module.exports = allowedOrigins;
